0c0b43769ec2c33cb3854637889d5c2becdaf4a6
Dhaneshwar Kumar <dhannesh@gmail.com>
2025-01-30 15:41:49 +0530

    Day4 completed with CRUD Operations

---
+++ day4/app.js
@@ -0,0 +1,51 @@
+import express from "express";
+
+import {
+  addProducts,
+  getProduct,
+  getProducts,
+  removeProduct,
+  updateProduct,
+} from "./utils.js";
+
+const app = express();
+app.use(express.json());
+
+app.get("/products", async (req, res) => {
+  const products = await getProducts();
+  res.send({ count: products.length, data: products });
+});
+
+app.post("/products", async (req, res) => {
+  const product = await addProducts(req.body);
+  res.send({ status: "success", data: product });
+});
+
+app.put("/products/:pid", async (req, res) => {
+  const pid = req.params.pid;
+  const product = req.body;
+  const oldProduct = await getProduct(pid);
+  if (oldProduct) {
+    await updateProduct(product, pid);
+    res.send({ status: "success", data: product });
+  } else {
+    res.send({ status: "error", msg: `product not found with id:${pid}` });
+  }
+});
+
+app.patch("/", async (req, res) => {
+  res.send("replace products");
+});
+
+app.delete("/products/:pid", async (req, res) => {
+  const pid = req.params.pid;
+  const product = await getProduct(pid);
+  if (product) {
+    await removeProduct(pid);
+    res.send({ status: "success", msg: "product removed" });
+  } else res.send({ status: "error", msg: `product not found with id:${pid}` });
+});
+
+app.listen(3000, () => {
+  console.log("Server is running...");
+});
---
+++ day4/utils.js
@@ -0,0 +1,62 @@
+import fsPromises from "node:fs/promises";
+
+const path = "./data.json";
+
+export const getProducts = async () => {
+  try {
+    const data = await fsPromises.readFile(path, "utf-8");
+    const products = await JSON.parse(data);
+    return products;
+  } catch (err) {
+    console.log("Error in reading file:", err.message);
+  }
+};
+
+export const addProducts = async (product) => {
+  try {
+    const products = await getProducts(path);
+    const id = Date.now();
+    const updatedProducts = [...products, { id, ...product }];
+    await fsPromises.writeFile(path, JSON.stringify(updatedProducts), "utf-8");
+    return product;
+  } catch (err) {
+    console.log("Error in product addition:", err.message);
+  }
+};
+
+export const getProduct = async (pid) => {
+  try {
+    const products = await getProducts(path);
+    const product = products.find((product) => product.id == pid);
+    return product;
+  } catch (err) {}
+};
+
+export const removeProduct = async (pid) => {
+  try {
+    const products = await getProducts(path);
+    const updatedProducts = products.filter((product) => product.id != pid);
+    await fsPromises.writeFile(path, JSON.stringify(updatedProducts), "utf-8");
+    return pid;
+  } catch (err) {
+    console.log("Error in removing product", err.message);
+  }
+};
+
+export const updateProduct = async (product, pid) => {
+  try {
+    const products = await getProducts(path);
+    const updatedProducts = products.map((item) => {
+      if (item.id == pid) {
+        console.log(item);
+        return { ...item, name: product.name, price: product.price };
+      }
+      return item;
+    });
+    console.log(updatedProducts);
+
+    await fsPromises.writeFile(path, JSON.stringify(updatedProducts), "utf-8");
+  } catch (err) {
+    console.log("Error in product update", err.message);
+  }
+};
